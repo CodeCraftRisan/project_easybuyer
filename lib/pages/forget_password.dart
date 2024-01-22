35936cdd8307f71070e4711b244fea31748c9d34
CodeCraftRisan <152704503+CodeCraftRisan@users.noreply.github.com>
2024-01-23 00:17:12 +0600

    forget password page created

--- lib/pages/forget_password.dart
+++ lib/pages/forget_password.dart
@@ -17,5 +17,81 @@ moveTologinpage(BuildContext context)async{
       changebutton = false;
     });
   }
+}@override
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Image.asset("assets/Images/forgot.png",fit: BoxFit.cover,),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Forget Password",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.black),),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: email,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Enter Your Email",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          ),
+                          labelText: "Email"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Email cannot be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    InkWell(
+                      onTap: ()=>moveTologinpage(context),
+                      child: AnimatedContainer(duration: Duration(seconds: 1),
+                        height: 50,
+                        width: changebutton?50:150,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular( changebutton ?50:8)
+                        ),
+                        child: changebutton ?Icon(Icons.done):Text("Confirm",style: TextStyle(
+                            fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white
+                        ),),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
+}
+
 
